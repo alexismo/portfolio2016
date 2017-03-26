eda40ef54c2272c2d4d49ddb03ea9867a9a846a2
Alexis Morin <the.alexis.morin@gmail.com>
2017-03-26 18:35:17 -0400

    add video background to AOP page

--- js/main.js
+++ js/main.js
@@ -28,6 +28,27 @@ function resizeIntroDiv(){
   }
 }
 
+var vidRatio = 16/9;
+function resizeBgVideo(){
+  var vid = document.getElementById("share-video");
+  if(vid != null){
+    var pageHeight = window.innerHeight;
+    var pageWidth = window.innerWidth;
+    if((pageWidth/pageHeight) > vidRatio){//ratio is wider than 16:9
+      vid.style.width = "100%";
+      vid.style.height = "auto";
+      vid.style.marginLeft = "0";
+    }else{
+      var widthOfVideoAtHeight = pageHeight * vidRatio;
+      var videoOverflow = widthOfVideoAtHeight - pageWidth;
+      var newMargin = -(videoOverflow/2);
+      vid.style.width = "auto";
+      vid.style.height = "100%";
+      vid.style.marginLeft = -(videoOverflow/2)+"px";
+    }
+  }
+}
+
 function setIntroDivHeight(div, height){
   div.style.height = height+"px";
 }
